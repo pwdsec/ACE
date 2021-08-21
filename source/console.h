2aaa6ca74f95f3e2c6b5fea8390ac9e4abd83835
pwd0kernel <79817991+pwd0kernel@users.noreply.github.com>
2021-08-21 19:34:06 -0230

    Create console.h

---
+++ source/console.h
@@ -0,0 +1,30 @@
+//
+// Created by pwd0kernel on 8/15/2021.
+//
+
+#ifndef ACE_CONSOLE_H
+#define ACE_CONSOLE_H
+
+#include <consoleapi2.h>
+#include <cstdio>
+
+namespace Console {
+    class Console_A {
+    private:
+    public:
+        static void AllowConsole(const char *cname, const char *window_name, const char *window_new_name) {
+            FILE *c;
+            AllocConsole();
+            freopen_s(&c, "CONOUT$", "w", stdout);
+            freopen_s(&c, "CONIN$", "r", stdin);
+            SetWindowTextA(FindWindowA(nullptr, window_name), window_new_name);
+            SetConsoleTitleA(cname);
+        };
+
+        static void ConsoleOutput(const char *text) {
+            printf_s("[Console]: %s \n", text);
+        }
+    };
+}
+
+#endif //ACE_CONSOLE_H
