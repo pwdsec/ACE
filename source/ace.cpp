5aa9479187bd17fb2743374af9cdf976e039ac84
pwd0kernel <79817991+pwd0kernel@users.noreply.github.com>
2021-08-22 14:34:48 -0230

    Update ace.cpp

--- source/ace.cpp
+++ source/ace.cpp
@@ -1,19 +1,16 @@
-#include <string>
-#include <iostream>
 #include <Windows.h>
 
 #include "core.h"
 #include "console.h"
 
 int __RPC_CALLEE main() {
-    Console::Console_A::AllowConsole("ACE", "AssaultCube", "ACE - 1.0");
-    std::string data;
-    Console::Console_A::ConsoleOutput("Loaded");
+    AllowConsole("ACE", "AssaultCube", "ACE - 1.0");
+    ConsoleOutput("Loaded");
     while (true) {
-        std::cout << "> ";
-        std::getline(std::cin, data);
-        if (data == "exit") return 0;
-        else AC_execute(data.c_str());
+        char data[99];
+        printf("> ");
+        fgets(data, 99, stdin);
+        AC_execute(data);
     }
 }
 
@@ -23,13 +20,13 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 ) {
     switch (ul_reason_for_call) {
         case DLL_PROCESS_ATTACH:
-            CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(main), nullptr, 0, nullptr);
+            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)main, NULL, 0, NULL);
         case DLL_THREAD_ATTACH:
         case DLL_THREAD_DETACH:
         case DLL_PROCESS_DETACH:
             break;
         default:
-            CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(main), nullptr, 0, nullptr);
+            CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)main, NULL, 0, NULL);
     }
     return TRUE;
 }
